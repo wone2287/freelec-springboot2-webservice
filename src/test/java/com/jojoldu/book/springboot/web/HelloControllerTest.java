52cb9cbb2a9d01ef302a1a980ff4e0a60f5f2d67
wone2287 <wone2287@gmail.com>
2020-02-20 23:39:28 +0900

    add HelloControllerTest

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 // test 를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킴
@@ -43,8 +43,22 @@ public class HelloControllerTest {
                 // 우리가 흔히 알고있는 200, 404, 500 등의 상태를 검증
                 // 여기선 OK, 즉 200이 아닌지 검증
                 .andExpect(content().string(hello));
-                // mvc.perform의 결과를 검증합니다.
-                // 응답 본문의 내용을 검증합니다
-                // Controller 에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
+        // mvc.perform의 결과를 검증합니다.
+        // 응답 본문의 내용을 검증합니다
+        // Controller 에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
+    }
+
+    @Test
+    public void helloDtoTest() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
