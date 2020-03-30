3e811d5187092d91373e5731122b41c5223cf3e7
wone2287 <wone2287@gmail.com>
2020-03-30 15:53:28 +0900

    Add mustache dependency

--- src/main/java/com/jojoldu/book/springboot/web/ExampleController.java
+++ src/main/java/com/jojoldu/book/springboot/web/ExampleController.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.web.Dto.ExampleResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,11 @@ public class ExampleController {
     @GetMapping("/")
     public ExampleResponseDto Example(@RequestParam String name, @RequestParam int age){
         return new ExampleResponseDto(name, age);
+    }
+
+    @GetMapping("/add/{id}")
+    public int add(@PathVariable int id){
+        int sum = id + 10;
+        return sum;
     }
 }
--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.springboot.web;
 
 import com.jojoldu.book.springboot.web.Dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,12 @@ public class HelloController {
 
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name,
-                                     @RequestParam("amount") int amount){
-                                    // 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션임.
-                                    // 여기서는 외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터
-                                    // 를 메소드 파라미터 name(String name)에 저장하게 됩니다.
+                                     @RequestParam("amount") int amount) {
+        // 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션임.
+        // 여기서는 외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터
+        // 를 메소드 파라미터 name(String name)에 저장하게 됩니다.
         return new HelloResponseDto(name, amount);
     }
+
+
 }
